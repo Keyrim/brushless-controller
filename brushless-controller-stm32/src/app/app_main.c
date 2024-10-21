1f0edc347c40e4b2182783486a82910b81c50e0c
Keyrim <40676844+Keyrim@users.noreply.github.com>
2024-10-21 23:28:22 +0200

    add basic clang format

---
+++ brushless-controller-stm32/src/app/app_main.c
@@ -0,0 +1,14 @@
+#include "app_main.h"
+
+/**
+ * @brief Initialize the main application
+ */
+void app_main_init(void) {}
+
+/**
+ * @brief Main application loop
+ */
+void app_main_loop(void) {
+    while (1) {
+    }
+}
---
+++ brushless-controller-stm32/src/app/app_main.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/**
+ * @brief Initialize the main application
+ */
+void app_main_init(void);
+
+/**
+ * @brief Main application loop
+ */
+void app_main_loop(void);
